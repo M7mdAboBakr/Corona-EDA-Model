55003bc7d7c83372912051d02c4e698e976322ae
Mohamed Abobakr <149151823+M7mdAboBakr@users.noreply.github.com>
2024-09-05 00:08:52 +0300

    Add files via upload

---
+++ pages/2_Bivariate & Multivariate.py
@@ -0,0 +1,190 @@
+import pandas as pd
+import plotly.express as px
+import streamlit as st
+
+df_main = pd.read_csv('Covid Data Cleaned.csv')
+
+st.set_page_config(
+    page_title="Bivariate & Multivariate",
+    page_icon="",
+    layout="wide",
+    )
+
+st.markdown("<h1 style='text-align: center; color: #ad79b5; font-size: 50px; font-weight: bold;'>Death Section</h1>", unsafe_allow_html=True)
+st.markdown("\n")
+
+col1, col2, col3 = st.columns(3)
+
+# gender
+col1.markdown("<h1 style='text-align: center; color: #ad79b5; font-size: 30px; font-weight: bold;'>Death Distribution with Gender</h1>", unsafe_allow_html=True)
+col1.markdown("\n")
+
+fig = px.sunburst(df_main.groupby('gender')['dead'].value_counts().reset_index(),
+                   path=['gender', 'dead'], values='count',
+                   color_discrete_sequence=px.colors.sequential.RdBu_r)
+fig.update_traces(marker=dict(line=dict(color='black', width=1)),
+                  textinfo="label+percent parent")
+col1.plotly_chart(fig, use_container_width=True)
+
+# patient type
+col2.markdown("<h1 style='text-align: center; color: #ad79b5; font-size: 30px; font-weight: bold;'>Death Distribution with Patient Type</h1>", unsafe_allow_html=True)
+col2.markdown("\n")
+
+fig = px.sunburst(df_main.groupby('patient_type')['dead'].value_counts().reset_index(),
+                   path=['patient_type', 'dead'], values='count',
+                   color_discrete_sequence=px.colors.sequential.RdBu_r)
+fig.update_traces(marker=dict(line=dict(color='black', width=1)),
+                  textinfo="label+percent parent")
+col2.plotly_chart(fig, use_container_width=True)
+
+
+col3.markdown("<h1 style='text-align: center; color: #ad79b5; font-size: 30px; font-weight: bold;'>Death Distribution with Pneumonia</h1>", unsafe_allow_html=True)
+fig = px.sunburst(df_main.groupby('pneumonia')['dead'].value_counts().reset_index(),
+                   path=['pneumonia', 'dead'], values='count',
+                   color_discrete_sequence=px.colors.sequential.RdBu_r)
+fig.update_traces(marker=dict(line=dict(color='black', width=1)),
+                  textinfo="label+percent parent")
+col3.plotly_chart(fig, use_container_width=True)
+
+col1, col2 = st.columns(2)
+
+# obesity
+col1.markdown("<h1 style='text-align: center; color: #ad79b5; font-size: 30px; font-weight: bold;'>Death Distribution with Obesity</h1>", unsafe_allow_html=True)
+col1.markdown("\n")
+fig = px.sunburst(df_main.groupby('obesity')['dead'].value_counts().reset_index(),
+                   path=['obesity', 'dead'], values='count', template='plotly_dark',
+                   color_discrete_sequence=px.colors.sequential.RdBu)
+fig.update_traces(marker=dict(line=dict(color='black', width=7)),
+                  textinfo="label+percent parent")
+col1.plotly_chart(fig, use_container_width=True)
+
+# classification
+col2.markdown("<h1 style='text-align: center; color: #ad79b5; font-size: 30px; font-weight: bold;'>Death Distribution with Diagnosis</h1>", unsafe_allow_html=True)
+col2.markdown("\n")
+fig = px.sunburst(df_main.groupby('classification')['dead'].value_counts().reset_index(),
+                   path=['classification', 'dead'], values='count', template='plotly_dark',
+                   color_discrete_sequence=px.colors.sequential.RdBu)
+fig.update_traces(marker=dict(line=dict(color='black', width=7)),
+                  textinfo="label+percent parent")
+col2.plotly_chart(fig, use_container_width=True)
+
+st.divider()
+
+
+# diagnosis section
+st.markdown("<h1 style='text-align: center; color: #ad79b5; font-size: 50px; font-weight: bold;'>Diagnosis Section</h1>", unsafe_allow_html=True)
+st.markdown("\n")
+
+
+col1, col2, col3 = st.columns(3)
+col1.markdown("<h1 style='text-align: center; color: #ad79b5; font-size: 30px; font-weight: bold;'>Diagnosis Distribution with Gender</h1>", unsafe_allow_html=True)
+col1.markdown("\n")
+
+# gender
+fig = px.sunburst(df_main.groupby('gender')['classification'].value_counts().reset_index(),
+                   path=['gender', 'classification'], values='count', template='plotly_dark',
+                   color_discrete_sequence=px.colors.sequential.RdBu)
+fig.update_traces(marker=dict(line=dict(color='black', width=7)),
+                  textinfo="label+percent parent")
+col1.plotly_chart(fig, use_container_width=True)
+
+
+col2.markdown("<h1 style='text-align: center; color: #ad79b5; font-size: 30px; font-weight: bold;'>Diagnosis Distribution with Patient Type</h1>", unsafe_allow_html=True)
+col2.markdown("\n")
+
+# patient type
+fig = px.sunburst(df_main.groupby('patient_type')['classification'].value_counts().reset_index(),
+                   path=['patient_type', 'classification'], values='count', template='plotly_dark',
+                   color_discrete_sequence=px.colors.sequential.RdBu)
+fig.update_traces(marker=dict(line=dict(color='black', width=7)),
+                  textinfo="label+percent parent")
+col2.plotly_chart(fig, use_container_width=True)
+
+
+col3.markdown("<h1 style='text-align: center; color: #ad79b5; font-size: 30px; font-weight: bold;'>Diagnosis Distribution with Pneumonia</h1>", unsafe_allow_html=True)
+col3.markdown("\n")
+
+# pneumonia
+fig = px.sunburst(df_main.groupby('pneumonia')['classification'].value_counts().reset_index(),
+                   path=['pneumonia', 'classification'], values='count', template='plotly_dark',
+                   color_discrete_sequence=px.colors.sequential.RdBu)
+fig.update_traces(marker=dict(line=dict(color='black', width=7)),
+                  textinfo="label+percent parent")
+col3.plotly_chart(fig, use_container_width=True)
+
+st.divider()
+
+# age section
+st.markdown("<h1 style='text-align: center; color: #ad79b5; font-size: 50px; font-weight: bold;'>Age Section</h1>", unsafe_allow_html=True)
+st.markdown("\n")
+
+
+
+# patient type
+st.markdown("<h1 style='text-align: center; color: #ad79b5; font-size: 30px; font-weight: bold;'>Age Distribution with Patient Type</h1>", unsafe_allow_html=True)
+st.markdown("\n")
+
+fig = px.histogram(df_main, x='age', color='patient_type', barmode='group',
+                   labels={'age': 'Age', 'patient_type': 'Patient Type'})
+st.plotly_chart(fig, use_container_width=True)
+
+# pneumonia
+st.markdown("<h1 style='text-align: center; color: #ad79b5; font-size: 30px; font-weight: bold;'>Age Distribution with Pneumonia</h1>", unsafe_allow_html=True)
+st.markdown("\n")
+
+fig = px.histogram(df_main, x='age', color='pneumonia', barmode='group',
+                   labels={'age': 'Age', 'pneumonia': 'Pneumonia'})
+st.plotly_chart(fig, use_container_width=True)
+
+
+# hypertension
+st.markdown("<h1 style='text-align: center; color: #ad79b5; font-size: 30px; font-weight: bold;'>Age Distribution with Hypertension</h1>", unsafe_allow_html=True)
+st.markdown("\n")
+fig = px.histogram(df_main, x='age', color='hypertension', barmode='group',
+                   labels={'age': 'Age', 'hypertension': 'Hypertension'})
+st.plotly_chart(fig, use_container_width=True)
+
+st.divider()
+
+# filter
+st.markdown("<h1 style='text-align: center; color: #ad79b5; font-size: 50px; font-weight: bold;'>Filter The Data</h1>", unsafe_allow_html=True)
+
+col1, col2 = st.columns(2)
+
+
+index = col1.selectbox(options=['gender', 'patient_type', 'pneumonia', 'diabetes',
+       'copd', 'asthma', 'inmsupr', 'hypertension', 'other_disease',
+       'cardiovascular', 'obesity', 'renal_chronic', 'tobacco',
+       'classification', 'dead'], label='Index (Inner Circle)')
+
+column = col2.selectbox(options=['gender', 'patient_type', 'pneumonia', 'diabetes',
+       'copd', 'asthma', 'inmsupr', 'hypertension', 'other_disease',
+       'cardiovascular', 'obesity', 'renal_chronic', 'tobacco',
+       'classification', 'dead'], label='Column (Outer Circle)')
+
+st.markdown("\n")
+if index == column:
+    st.warning('Please select two different columns')
+
+else:
+    col1.markdown("\n")
+    col1.markdown("\n")
+    col1.markdown("\n")
+    col1.markdown("\n")
+    col1.markdown("\n")
+    col1.markdown("\n")
+    col1.markdown("\n")
+    col1.markdown("\n")
+    col1.markdown("\n")
+    col1.markdown("\n")
+    col1.markdown("\n")
+    col1.dataframe(df_main.pivot_table(index=index, columns=column, values='age', aggfunc='count'))
+
+    fig = px.sunburst(df_main.groupby(index)[column].value_counts().reset_index(), path=[index, column], values='count', template='plotly_dark',
+                 color_discrete_sequence=px.colors.sequential.RdBu)
+    fig.update_traces(marker=dict(line=dict(color='black', width=7)), textinfo="label+percent parent")
+
+    col2.plotly_chart(fig, use_container_width=True)
+  
+
+
---
+++ pages/3_The Model.py
@@ -0,0 +1,91 @@
+import pandas as pd
+import pickle
+import streamlit as st
+import time
+from streamlit_lottie import st_lottie
+import requests
+
+st.set_page_config(
+    page_title="The Model",
+    page_icon="",
+    layout="wide",
+    )
+
+def load_lottieurl(url:str):
+    r = requests.get(url)
+    if r.status_code !=200:
+        return None
+    return r.json()
+
+animation_1 = load_lottieurl('https://lottie.host/60b6d659-4cf3-4678-8ce4-382a4023bf14/MaEyxklSaK.json')
+
+
+# st.markdown("<h1 style='text-align: center; color: #ad79b5; font-size: 50px; font-weight: bold;'>The Model</h1>", unsafe_allow_html=True)
+st_lottie(animation_1, speed=1, height=400, loop=True, quality="high")
+
+age = st.slider('Age', 0, 100, 25)
+
+col1, col2 = st.columns(2)
+
+preprocessor, threshold = pickle.load(open('preprocessor_threshold.pkl', 'rb'))
+model = pickle.load(open('model.pkl', 'rb')) 
+
+gender = col2.radio('Gender', ('Male', 'Female'))
+
+patient_type = col1.radio('Patient Type', ('Home', 'Hospitalized'))
+
+pneumonia = col2.radio('Pneumonia', ("No Pneumonia", "Pneumonia"))
+
+dead = col1.radio('Death', ("Not Dead", "Dead"))
+
+tobacco = col2.radio('Tobacco', (('No Tobacco', 'Tobacco')))
+
+diabetes = col1.radio('Diabetes', ('No Diabetes', 'Diabetes'))
+
+copd = col2.radio('COPD', ('No COPD', 'COPD'))
+
+asthma = col1.radio('Asthma', ('No Asthma', 'Asthma'))
+
+inmsupr = col2.radio('Inmsupr', ('Not Inmsupr', 'Inmsupr'))
+
+hypertension = col1.radio('Hypertension', ('No Hypertension', 'Hypertension'))
+
+other_disease = col2.radio('Other Disease', ('No', 'Yes'))
+
+cardiovascular = col1.radio('Cardiovascular', ('Not Cardiovascular', 'Cardiovascular'))
+
+renal_chronic = st.radio('Renal Chronic', ('No Renal Chronic', 'Renal Chronic'))
+
+# order them like than
+prediction = pd.DataFrame({'gender': [gender], 'patient_type': [patient_type], 'pneumonia': [pneumonia], 
+ 'age': [age], 'diabetes': [diabetes], 'copd': [copd], 'asthma': [asthma], 
+ 'inmsupr': [inmsupr], 'hypertension': [hypertension], 'other_disease': [other_disease], 
+ 'cardiovascular': [cardiovascular], 'obesity':['Obesity'], 'renal_chronic': [renal_chronic], 
+ 'tobacco': [tobacco], 'dead': [dead]})
+
+
+prediction_preprocessed = pd.DataFrame(preprocessor.transform(prediction), columns=prediction.columns)
+prediction_preprocessed.drop('obesity', axis=1, inplace=True)
+
+if st.button('Predict'):
+
+    y_proba = model.predict_proba(prediction_preprocessed)
+    y_pred = (y_proba[:, 1] >= threshold).astype(int)
+
+    progress_text = "Operation in progress. Please wait."
+    my_bar = st.progress(0, text=progress_text)
+
+    for percent_complete in range(100):
+        time.sleep(0.01)
+        my_bar.progress(percent_complete + 1, text=progress_text)
+    time.sleep(1)
+    my_bar.empty()
+
+    if y_pred == 0:
+        st.success("No Corona Virus")
+
+    else:
+        st.error("Coronavirus Detected")
+
+    # st.markdown(f"<h1 style='text-align: center; color: #ad79b5; font-size: 50px; font-weight: bold;'>{y_pred[0]}</h1>", unsafe_allow_html=True)
+
